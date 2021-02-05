4163d020505f948c4b7df25628e713b8fbc292b1
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-02-05 16:26:39 +0100

    :3

--- food.js
+++ food.js
@@ -2,11 +2,11 @@ import { onSnake, expandSnake } from './snake.js'
 import { randomGridPosition } from './grid.js'
 
 let food = getRandomFoodPosition()
-const EXPANSION_RATE = 5
+const expansionRate = 5
 
 export function update() {
   if (onSnake(food)) {
-    expandSnake(EXPANSION_RATE)
+    expandSnake(expansionRate)
     food = getRandomFoodPosition()
   }
 }
